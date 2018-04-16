5d1ab73e5d6ae09aa34c8664c54f221b1ba31580
Joe Burghardt <jmburghardt@gmail.com>
2018-04-16 18:46:05 -0500

    lightly fuctioning site

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Post(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(255))
     completed = db.Column(db.Boolean)
 
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
         self.completed = False
@@ -25,29 +25,28 @@ class Post(db.Model):
 def index():
 
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Post(task_name)
-        db.session.add(new_task)
+        blog_title = request.form['blog_title']
+        blog_entry = request.form['blog_entry']
+        new_post = Blog(blog_title, blog_entry)
+        db.session.add(new_post)
         db.session.commit()
-    tasks = []
-    #tasks = Post.query.filter_by(completed=False).all()
-    #completed_tasks = Post.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks)
-        #, completed_tasks=completed_tasks)
+    
+    posts = Blog.query.filter_by(completed=False).all()
+    completed_post = Blog.query.filter_by(completed=True).all()
+    return render_template('blog.html',title="Build A Blog", 
+        posts=posts, completed_post=completed_post)
 
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
 
-    task_id = int(request.form['task-id'])
-    task = Post.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
+    #post_id = int(request.form['post-id'])
+    #post = Blog.query.get(post_id)
+    #post.completed = True
+    #db.session.add(post)
+    #db.session.commit()
 
-    return redirect('/')
-
+    return render_template('newpost.html', title='Build A Blog')
 
 if __name__ == '__main__':
     app.run()
