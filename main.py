964df2c530ecc8297fdf8d6d104ffd7830c2bb5b
Joe Burghardt <jmburghardt@gmail.com>
2018-04-16 17:19:03 -0500

    added templates

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(255))
     completed = db.Column(db.Boolean)
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title):
+        self.title = title
+        self.body = body
         self.completed = False
 
 
@@ -24,21 +26,22 @@ def index():
 
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Post(task_name)
         db.session.add(new_task)
         db.session.commit()
-
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    tasks = []
+    #tasks = Post.query.filter_by(completed=False).all()
+    #completed_tasks = Post.query.filter_by(completed=True).all()
     return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+        tasks=tasks)
+        #, completed_tasks=completed_tasks)
 
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
     task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
+    task = Post.query.get(task_id)
     task.completed = True
     db.session.add(task)
     db.session.commit()
